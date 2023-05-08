9197493b17cd9920b8c07e8dd0d597aede02344d
subhash9758964966 <subhashsagar98@gmail.com>
2023-05-08 13:59:20 +0530

    second commit

---
+++ download_water_logging_images.py
@@ -0,0 +1,4 @@
+from simple_image_download import simple_image_download as simp
+response = simp.simple_image_download
+keyword = "water logging in school"
+response().download(keyword, 100)
---
+++ download_without_water_logging_images .py
@@ -0,0 +1,4 @@
+from simple_image_download import simple_image_download as simp
+response = simp.simple_image_download
+keyword = "indian village school campus"
+response().download(keyword, 100)
